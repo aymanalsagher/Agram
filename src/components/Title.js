4fb68010544bbd46ad35eebddbc7af7dfc57b5b9
aymanalsagher <aymanalsagher@gmail.com>
2021-07-07 14:11:24 +0200

    Initial Version V0.2

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,9 @@
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
+      <h1>AGram</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Upload and share your moments now</p>
     </div>
   );
 };
